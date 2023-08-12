66864a663fb6306815d98ed88b5e7d60e5b29598
Georgiana <ion.georgiana@hotmail.com>
2023-08-12 19:39:04 +0300

    inputs

--- src/components/ConverterContainer.vue
+++ src/components/ConverterContainer.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="currency-converter-container">
         <div class="tabs">
-            <button @click="activeTab = 'convert'"
-                :class="{ active: activeTab === 'convert', radiusLeft: true }">Convert</button>
-            <button @click="activeTab = 'charts'"
-                :class="{ active: activeTab === 'charts', radiusRight: true }">Charts</button>
+            <button @click="activeTab = 'convert'" :class="{ active: activeTab === 'convert', radiusLeft: true }"><i
+                    class="fas fa-exchange-alt"></i>Convert</button>
+            <button @click="activeTab = 'charts'" :class="{ active: activeTab === 'charts', radiusRight: true }"><i
+                    class="fas fa-chart-line"></i>Charts</button>
         </div>
-        <div v-if="activeTab === 'convert'">
-            <!-- Place your Convert component or code here -->
+        <div v-if="activeTab === 'convert'" class="convert-tab">
+            <div class="converter-inputs">
+                <input class="from-input" type="text" v-model="fromCurrency" placeholder="From" />
+                <i class="fa-solid fa-repeat reverse-icon" @click="reverseCurrencies"></i>
+                <input class="to-input" type="text" v-model="toCurrency" placeholder="To" />
+            </div>
         </div>
         <div v-else-if="activeTab === 'charts'">
             <!-- Place your Charts component or code here -->
@@ -19,14 +23,25 @@
 import { ref } from 'vue';
 
 const activeTab = ref('convert');
+
+const fromCurrency = ref('');
+const toCurrency = ref('');
+
+const reverseCurrencies = () => {
+    const temp = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = temp;
+};
 </script>
   
 <style scoped>
 .currency-converter-container {
-    background-color: #f0f0f0;
+    background-color: #ECF0FF;
     width: 75rem;
     margin: auto;
     border-radius: 1.25rem;
+    border: 1px solid #5BC0F8;
+
 }
 
 .tabs {
@@ -48,7 +63,7 @@ const activeTab = ref('convert');
 }
 
 .tabs button.active {
-    background-color: #f0f0f0;
+    background-color: #ECF0FF;
     color: black !important;
 }
 
@@ -58,6 +73,37 @@ const activeTab = ref('convert');
 
 .radiusRight {
     border-radius: 0 1.25rem 0 0;
+}
+
+.tabs button i {
+    margin-right: 0.5rem;
+    font-size: 1.25rem;
+}
+
+.convert-tab {
+    padding: 4rem;
+}
+
+.converter-inputs {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.from-input,
+.to-input {
+    flex: 1;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.63rem;
+    height: 3.9rem;
+    border: 1px solid #5BC0F8;
+}
+
+.reverse-icon {
+    margin: 0 1rem;
+    font-size: 1.5rem;
+    cursor: pointer;
 }
 </style>
   
---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+    <input v-model="value" :type="type" :placeholder="placeholder" class="" />
+</template>
+  
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const { value, type, placeholder } = defineProps(['value', 'type', 'placeholder']);
+const { emit } = defineEmits();
+
+return {
+    value,
+    type,
+    placeholder,
+    emit,
+};
+</script>
+
+<style></style>
